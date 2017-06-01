ee5dd87be40d07aac887e21a5e0fc6da1def5ccb
Sean McKiernan <Sean McKiernan>
2017-05-31 19:19:13 -0500

    committing checkpoint-12-assignment

---
+++ scripts/album.js
@@ -0,0 +1,119 @@
+// Example Album
+var albumPicasso = {
+    title: 'The Colors', 
+    artist: 'Pablo Picasso', 
+    label: 'Cubism', 
+    year: '1881', 
+    albumArtUrl: 'assets/images/album_covers/01.png', 
+    songs: [
+        { title: 'Blue', duration: '4:26'}, 
+        { title: 'Green', duration: '3:14'}, 
+        { title: 'Red', duration: '5:01'}, 
+        { title: 'Pink', duration: '3:21'}, 
+        { title: 'Magenta', duration: '2:15'}
+    ]
+}; 
+
+// Another Example Album
+
+var albumMarconi = {
+    title: 'The Telephone', 
+    artist: 'Guglielmo Marconi', 
+    label: 'EM', 
+    year: '1990', 
+    albumArtUrl: 'assets/images/album_covers/20.png', 
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01'}, 
+        { title: 'Ring, ring, ring', duration: '5:01'}, 
+        { title: 'Fits in your pocket', duration: '3:21'}, 
+        { title: 'Can you hear me now?', duration: '3:14'}, 
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+}; 
+
+var albumHero = {
+    title: 'Prometheus', 
+    artist: 'Quake of the Universe', 
+    label: 'Titan', 
+    year: '2900 B.C.', 
+    albumArtUrl: 'assets/images/album_covers/07.png', 
+    songs: [
+        { title: 'The beginning', duration: '00:01'}, 
+        { title: 'Birth', duration: '5:05'}, 
+        { title: 'Battle', duration: '66:06'}, 
+        { title: 'Aftermath', duration: '20:05'}, 
+        { title: 'Re-Birth', duration: '00:30'}
+    ]
+};
+
+var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+      +' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
+
+ //#1
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0]; 
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0]; 
+    var albumImage = document.getElementsByClassName('album-cover-art')[0]; 
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0]; 
+
+var setCurrentAlbum = function(album) {
+   
+    // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+ 
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0]; 
+var songRows = document.getElementsByClassName('album-view-song-item'); 
+
+var playButtonTemplate = '<a class ="album-song-button"><span class="ion-play></span></a>'; 
+
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+     
+     songListContainer.addEventListener('mouseover', function(event) {
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             // Change the content from the number to the play button's HTML
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate; 
+         }
+     });
+     
+     for (var i= 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+                // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');     
+        });
+     }
+     
+     var albums = [albumPicasso, albumMarconi, albumHero]; 
+     var index = 1; 
+     
+     albumImage.addEventListener('click', function(Event) {
+         setCurrentAlbum(albums[index]);
+         index++; 
+            if (index == albums.length){
+                index=0; 
+            }
+     }); 
+     
+}
