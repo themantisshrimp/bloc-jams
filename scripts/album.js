ae0689177e5c2335da1bc3f9c53306fed6edb0f2
Sean McKiernan <Sean McKiernan>
2017-06-05 14:08:50 -0500

    edited checkpoint-12

--- scripts/album.js
+++ scripts/album.js
@@ -82,10 +82,15 @@ var setCurrentAlbum = function(album) {
      }
  };
 
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0]; 
 var songRows = document.getElementsByClassName('album-view-song-item'); 
 
-var playButtonTemplate = '<a class ="album-song-button"><span class="ion-play></span></a>'; 
+var playButtonTemplate = '<a class ="album-song-button"><span class="ion-play"></span></a>'; 
+var pauseButtonTemplate = '<a class ="album-song-button"><span class="ion-pause"></span></a>'; 
+
+// Store state of playing songs
+var currentlyPlayingSong = null; 
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
