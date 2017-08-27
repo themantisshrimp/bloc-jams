bc5757264710b577a77e7ff3ac014e31439ec8fe
Sean McKiernan <14mckiernans@gmail.com>
2017-08-27 13:38:56 -0500

    checkpoint-16 edit

--- scripts/landing.js
+++ scripts/landing.js
@@ -14,7 +14,7 @@ $(window).load(function () {
     if ($(window).height() > 950) {
         animatePoints();
     }
-    var scrollDistance = $('.selling.points').offset().top - $(window).height() + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
     $(window).scroll(function(event) {
         if ($(window).scrollTop() >= scrollDistance) {
