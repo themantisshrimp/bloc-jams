c70794c25822e9bfb19d95cc5adc7e7f7fbdc112
Sean McKiernan <14mckiernans@gmail.com>
2017-08-27 19:02:07 -0500

    checkpoint-17

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function () {
+    var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -14,14 +15,18 @@ var collectionItemTemplate =
    + '</div>'
    ;
 
-window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
- 
-     // #3
-     for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
-     }
- }
+     return $(template); 
+};
+$(window).load(function () {
+    // #1
+    var $collectionContainer = $('.album-covers');
+    // #2
+    $collectionContainer.empty(); 
+
+    // #3
+    for (var i = 0; i < 12; i++) {
+        var $newThumbnail = buildCollectionItemTemplate(); 
+
+        $collectionContainer.append($newThumbnail); 
+    }
+});
